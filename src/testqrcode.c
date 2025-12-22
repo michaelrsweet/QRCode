c9eae3680d4860d84ad525bf3765e07feaf2d8aa
Michael R Sweet <msweet@msweet.org>
2025-12-21 22:09:13 -0500

    Update testqrcode to support writing PNG files (by default).

--- src/testqrcode.c
+++ src/testqrcode.c
@@ -37,11 +37,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include "qrcode.h"
+#include <zlib.h>
+
+
+// PNG constants...
+#define PNG_PADDING  4                  // White padding around QR code
 
 
 // SVG constants...
 #define SVG_SCALE    5                  // Nominal size of modules
 #define SVG_PADDING  4                  // White padding around QR code
+
+
+// Local function for PNG output...
+static unsigned char *png_add_crc(unsigned char *pngdata, unsigned char *pngptr, unsigned char *pngend);
+static unsigned char *png_add_unsigned(unsigned val, unsigned char *pngptr, unsigned char *pngend);
 
 
 // Main entry
@@ -54,6 +64,8 @@ int main(int argc, char *argv[]) {
     QRCode     qrcode;                  // QR code data
     uint8_t    qrcodeBytes[qrcode_getBufferSize(VERSION_MAX)];
                                         // QR code buffer
+    bool       makeSVG = false;         // Output SVG?
+
 
     // Parse command-line...
     if ((progname = strrchr(argv[0], '/')) != NULL)
@@ -81,6 +93,21 @@ int main(int argc, char *argv[]) {
                         } else {
                             fprintf(stderr, "%s: Bad error correction level '-e %s'.\n", progname, argv[i]);
                             return 1;
+                        }
+                        break;
+
+                    case 'f' : /* -f FORMAT */
+                        i ++;
+                        if (i >= argc) {
+                            fprintf(stderr, "%s: Missing format after '-f'.\n", progname);
+                            return 1;
+                        } else {
+                            if (!strcmp(argv[i], "svg")) {
+                                makeSVG = true;
+                            } else if (strcmp(argv[i], "png")) {
+                                fprintf(stderr, "%s: Unsupported format '%s'.\n", progname, argv[i]);
+                                return 1;
+                            }
                         }
                         break;
 
@@ -127,28 +154,221 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // Write SVG to stdout...
-    printf("<svg width=\"%d\" height=\"%d\" xmlns=\"http://www.w3.org/2000/svg\">\n", (qrcode.size + 2 * SVG_PADDING) * SVG_SCALE, (qrcode.size + 2 * SVG_PADDING) * SVG_SCALE);
-    printf("  <rect x=\"0\" y=\"0\" width=\"%d\" height=\"%d\" fill=\"white\" />\n", (qrcode.size + 2 * SVG_PADDING) * SVG_SCALE, (qrcode.size + 2 * SVG_PADDING) * SVG_SCALE);
+    if (makeSVG) {
+	// Write SVG to stdout...
+	printf("<svg width=\"%d\" height=\"%d\" xmlns=\"http://www.w3.org/2000/svg\">\n", (qrcode.size + 2 * SVG_PADDING) * SVG_SCALE, (qrcode.size + 2 * SVG_PADDING) * SVG_SCALE);
+	printf("  <rect x=\"0\" y=\"0\" width=\"%d\" height=\"%d\" fill=\"white\" />\n", (qrcode.size + 2 * SVG_PADDING) * SVG_SCALE, (qrcode.size + 2 * SVG_PADDING) * SVG_SCALE);
 
-    for (uint8_t y = 0; y < qrcode.size; y++) {
-        uint8_t xstart = 0, xcount = 0;
+	for (uint8_t y = 0; y < qrcode.size; y++) {
+	    uint8_t xstart = 0, xcount = 0;
 
-        for (uint8_t x = 0; x < qrcode.size; x++) {
-            if (qrcode_getModule(&qrcode, x, y)) {
-                if (xcount == 0) { xstart = x; }
-                xcount ++;
-            } else if (xcount > 0) {
-                printf("  <rect x=\"%d\" y=\"%d\" width=\"%d\" height=\"%d\" fill=\"black\" />\n", (xstart + SVG_PADDING) * SVG_SCALE, (y + SVG_PADDING) * SVG_SCALE, xcount * SVG_SCALE, SVG_SCALE);
-                xcount = 0;
+	    for (uint8_t x = 0; x < qrcode.size; x++) {
+		if (qrcode_getModule(&qrcode, x, y)) {
+		    if (xcount == 0) { xstart = x; }
+		    xcount ++;
+		} else if (xcount > 0) {
+		    printf("  <rect x=\"%d\" y=\"%d\" width=\"%d\" height=\"%d\" fill=\"black\" />\n", (xstart + SVG_PADDING) * SVG_SCALE, (y + SVG_PADDING) * SVG_SCALE, xcount * SVG_SCALE, SVG_SCALE);
+		    xcount = 0;
+		}
+	    }
+
+	    if (xcount > 0) {
+		printf("  <rect x=\"%d\" y=\"%d\" width=\"%d\" height=\"%d\" fill=\"black\" />\n", (xstart + SVG_PADDING) * SVG_SCALE, (y + SVG_PADDING) * SVG_SCALE, xcount * SVG_SCALE, SVG_SCALE);
+	    }
+	}
+
+	puts("</svg>");
+    } else {
+        // Write PNG to stdout...
+        unsigned char	pngbuf[65536],	// PNG output buffer
+			*pngptr = pngbuf,
+					// Pointer into PNG buffer
+			*pngend = pngbuf + sizeof(pngbuf),
+					// Pointer to end of PNG buffer
+			*pngdata,	// Start of PNG chunk data
+			line[1 + (255 + 2 * PNG_PADDING + 7) / 8],
+					// PNG bitmap line starting with filter byte
+			*lineptr,	// Pointer into line
+			bit;		// Current bit
+        unsigned	size = qrcode.size + 2 * PNG_PADDING,
+					// Size of image
+			linelen = (size + 7) / 8,
+					// Length of a line
+			x, y;		// Looping vars
+        int		zerr;		// ZLIB error code
+	z_stream	zstream;	// ZLIB compression stream
+
+
+        // Add the PNG file header...
+        *pngptr++ = 137;
+        *pngptr++ = 80;
+        *pngptr++ = 78;
+        *pngptr++ = 71;
+        *pngptr++ = 13;
+        *pngptr++ = 10;
+        *pngptr++ = 26;
+        *pngptr++ = 10;
+
+        // Add the IHDR chunk...
+        pngptr    = png_add_unsigned(13, pngptr, pngend);
+        pngdata   = pngptr;
+
+        *pngptr++ = 'I';
+        *pngptr++ = 'H';
+        *pngptr++ = 'D';
+        *pngptr++ = 'R';
+
+        pngptr    = png_add_unsigned(size, pngptr, pngend);
+					// Width
+        pngptr    = png_add_unsigned(size, pngptr, pngend);
+					// Height
+        *pngptr++ = 1;			// Bit depth
+        *pngptr++ = 0;			// Color type grayscale
+        *pngptr++ = 0;			// Compression method 0 (deflate)
+        *pngptr++ = 0;			// Filter method 0 (adaptive)
+        *pngptr++ = 0;			// Interlace method 0 (no interlace)
+        pngptr    = png_add_crc(pngdata, pngptr, pngend);
+
+        // Add the IDAT chunk...
+        pngptr    += 4;			// Leave room for length
+        pngdata   = pngptr;
+
+        *pngptr++ = 'I';
+        *pngptr++ = 'D';
+        *pngptr++ = 'A';
+        *pngptr++ = 'T';
+
+        // Initialize zlib compressor...
+        if ((zerr = deflateInit2(&zstream, Z_DEFAULT_COMPRESSION, Z_DEFLATED, /*windowBits*/11, /*memLevel*/7, Z_DEFAULT_STRATEGY)) < Z_OK) {
+            fprintf(stderr, "%s: Unable to create deflate stream (%d).\n", progname, zerr);
+            return 1;
+        }
+
+        zstream.next_in   = (Bytef *)line;
+        zstream.next_out  = (Bytef *)pngptr;
+        zstream.avail_out = (uInt)(sizeof(pngbuf) - (pngptr - pngbuf));
+
+        // All lines start with the "None" (0) filter...
+        line[0] = 0;
+
+        // Add padding at the top...
+        memset(line + 1, 0xff, linelen);
+        for (y = 0; y < PNG_PADDING; y ++) {
+	    zstream.next_in  = (Bytef *)line;
+            zstream.avail_in = linelen + 1;
+            if ((zerr = deflate(&zstream, Z_NO_FLUSH)) < Z_OK) {
+                fprintf(stderr, "%s: Unable to deflate image (%d).\n", progname, zerr);
+                return 1;
             }
         }
 
-        if (xcount > 0) {
-            printf("  <rect x=\"%d\" y=\"%d\" width=\"%d\" height=\"%d\" fill=\"black\" />\n", (xstart + SVG_PADDING) * SVG_SCALE, (y + SVG_PADDING) * SVG_SCALE, xcount * SVG_SCALE, SVG_SCALE);
+        // Add lines from the QR code...
+        for (y = 0; y < qrcode.size; y ++) {
+	    memset(line + 1, 0xff, linelen);
+
+	    for (x = 0, lineptr = line + 1, bit = 128 >> PNG_PADDING; x < qrcode.size; x ++) {
+		if (qrcode_getModule(&qrcode, x, y)) {
+		    *lineptr ^= bit;
+		}
+
+		if (bit == 1) {
+		    lineptr ++;
+		    bit = 128;
+		} else {
+		    bit = bit / 2;
+		}
+	    }
+
+	    zstream.next_in  = (Bytef *)line;
+            zstream.avail_in = linelen + 1;
+            if ((zerr = deflate(&zstream, Z_NO_FLUSH)) < Z_OK) {
+                fprintf(stderr, "%s: Unable to deflate image (%d).\n", progname, zerr);
+                return 1;
+            }
         }
+
+        // Add padding at the bottom...
+        memset(line + 1, 0xff, linelen);
+        for (y = 0; y < PNG_PADDING; y ++) {
+	    zstream.next_in  = (Bytef *)line;
+            zstream.avail_in = linelen + 1;
+            if ((zerr = deflate(&zstream, Z_NO_FLUSH)) < Z_OK) {
+                fprintf(stderr, "%s: Unable to deflate image (%d).\n", progname, zerr);
+                return 1;
+            }
+        }
+
+        // Finish compression...
+	zstream.next_in  = (Bytef *)line;
+	zstream.avail_in = 0;
+	if ((zerr = deflate(&zstream, Z_FINISH)) != Z_STREAM_END) {
+	    fprintf(stderr, "%s: Unable to end image (%d).\n", progname, zerr);
+	    return 1;
+	}
+
+        pngptr = (unsigned char *)zstream.next_out;
+
+        png_add_unsigned((unsigned)(pngptr - pngdata - 4), pngdata - 4, pngend);
+        pngptr = png_add_crc(pngdata, pngptr, pngend);
+
+        // Add the IEND chunk...
+        pngptr  = png_add_unsigned(0, pngptr, pngend);
+        pngdata = pngptr;
+
+        *pngptr++ = 'I';
+        *pngptr++ = 'E';
+        *pngptr++ = 'N';
+        *pngptr++ = 'D';
+
+        pngptr    = png_add_crc(pngdata, pngptr, pngend);
+
+        // Write the PNG file to stdout...
+        fwrite(pngbuf, (size_t)(pngptr - pngbuf), 1, stdout);
+        fflush(stdout);
     }
 
-    puts("</svg>");
     return 0;
+}
+
+
+//
+// 'png_add_crc()' - Compute and append the chunk data CRC.
+//
+
+static unsigned char *			// O - Next byte in output buffer
+png_add_crc(unsigned char *pngdata,	// I - Pointer to start of chunk data
+            unsigned char *pngptr,	// I - Pointer to end of chunk data (where CRC goes)
+            unsigned char *pngend)	// I - Pointer to end of PNG output buffer
+{
+  unsigned		c;		// CRC value
+
+
+  c = crc32(0, Z_NULL, 0);
+  c = crc32(c, pngdata, (uInt)(pngptr - pngdata));
+
+  // Append the CRC to the buffer...
+  return (png_add_unsigned(c, pngptr, pngend));
+}
+
+
+//
+// 'png_add_unsigned()' - Add a 32-bit unsigned integer to the PNG buffer.
+//
+
+static unsigned char *			// O - Next byte in output buffer
+png_add_unsigned(unsigned      val,	// I - Value to append
+                 unsigned char *pngptr,	// I - Pointer into output buffer
+                 unsigned char *pngend)	// I - Pointer to end of output buffer
+{
+  // Append the value to the buffer...
+  if (pngptr < pngend)
+    *pngptr++ = (val >> 24) & 0xff;
+  if (pngptr < pngend)
+    *pngptr++ = (val >> 16) & 0xff;
+  if (pngptr < pngend)
+    *pngptr++ = (val >> 8) & 0xff;
+  if (pngptr < pngend)
+    *pngptr++ = val & 0xff;
+
+  return (pngptr);
 }
